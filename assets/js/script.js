cf49bd0d7d0fae3bbb60ea8af0da78a9c16f3aaa
Ambernina <rob.ambernina@gmail.com>
2019-12-02 21:15:43 -0700

    added js css and images

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.datepicker').datepicker();
+});
