10494d13dbdf6ac59da431a4af47c1f285201c3f
Ambernina <rob.ambernina@gmail.com>
2019-12-04 18:32:14 -0700

    added google maps links and scripts added yelp website link to footer and sizing to map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
+(function($) {
+	$(function() {
+		$(".sidenav").sidenav();
+		$(".parallax").parallax();
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
 
 // Or with jQuery
 
-$(document).ready(function(){
-  $('.datepicker').datepicker();
+$(document).ready(function() {
+	$(".datepicker").datepicker();
 });
+
+// GOOGLE MAPS API CALL
+// lat and lon will later be put in vars and set to the responses lat and lon
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById("map"), {
+		center: { lat: -34.397, lng: 150.644 },
+		zoom: 8
+	});
+}
