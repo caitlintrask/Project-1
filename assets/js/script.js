114012b33c33246c2744025cae367619f3826f2a
Ambernina <rob.ambernina@gmail.com>
2019-12-04 18:20:57 -0700

    added google maps api call to be specified later

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
+(function($) {
+	$(function() {
+		$(".sidenav").sidenav();
+		$(".parallax").parallax();
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
 
 // Or with jQuery
 
-$(document).ready(function(){
-  $('.datepicker').datepicker();
+$(document).ready(function() {
+	$(".datepicker").datepicker();
 });
+
+// Google maps API call
+
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById("map"), {
+		center: { lat: -34.397, lng: 150.644 },
+		zoom: 8
+	});
+}
