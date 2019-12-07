176811d77408ff6c9a7fdaffd8232fde7a17a6d8
Ambernina <rob.ambernina@gmail.com>
2019-12-07 12:11:35 -0700

    added if else statements for rating stars ;

--- script.js
+++ script.js
@@ -31,57 +31,57 @@ $.ajax({
 });
 
 // "Restaurant" API Call
-
-// need to figure out how to get the restaurant id from the responses of restaurant options and input that in the URL here
-
-// var restaurantID = 
-var restaurantQueryURL = "https://developers.zomato.com/api/v2.1/restaurant?res_id=16774318";
-
-$.ajax({
-    url: restaurantQueryURL,
-    headers:{ 'user-key': APIKey },
-    method: "GET"
-})
-    .then(function(response) {
-    console.log(response);
-    
-    // restaurant contact info
-    console.log("restaurant name: " + response.name);
-    console.log("restaurant cuisine: " + response.cuisines);
-    console.log("address: " + response.location.address);
-    console.log("phone number: " + response.phone_numbers);
-
-    // restaurant website links
-    console.log("website: " + response.url);
-    console.log("menu: " + response.menu_url);
-
-    // restaurant rating and price info
-    console.log("rating text: " + response.user_rating.rating_text);
-    console.log("aggregate rating: " + response.user_rating.aggregate_rating);
-    console.log("cost for two: " + response.average_cost_for_two);
-
-    // restaurant photos
-    console.log("feautured photo url: " + response.thumb);
-    console.log("photo gallery urls: " , response.photos);
-})
-
-// RESTAURANT IDs
-var searchResQueryURL = "https://developers.zomato.com/api/v2.1/search"
-
-$.ajax({
-    url: searchResQueryURL,
-    headers:{ 'user-key': APIKey },
-    method: "GET"
-})
-    .then(function(response) {
-    console.log(response);
-})
-
-var ratingTextQueryURL =
+var restaurantQueryURL =
 	"https://developers.zomato.com/api/v2.1/restaurant?res_id=16774318";
 
 $.ajax({
-	url: ratingTextQueryURL,
+	url: restaurantQueryURL,
+	headers: { "user-key": APIKey },
+	method: "GET"
+}).then(function(response) {
+	console.log(response);
+
+	// restaurant contact info
+	console.log("restaurant name: " + response.name);
+	console.log("restaurant cuisine: " + response.cuisines);
+	console.log("address: " + response.location.address);
+	console.log("phone number: " + response.phone_numbers);
+
+	// restaurant website links
+	console.log("website: " + response.url);
+	console.log("menu: " + response.menu_url);
+
+	// restaurant rating and price info
+	console.log("rating text: " + response.user_rating.rating_text);
+	console.log("aggregate rating: " + response.user_rating.aggregate_rating);
+	console.log("cost for two: " + response.average_cost_for_two);
+
+	// restaurant photos
+	console.log("feautured photo url: " + response.thumb);
+	console.log("photo gallery urls: ", response.photos);
+});
+
+// RESTAURANT IDs
+var searchResQueryURL = "https://developers.zomato.com/api/v2.1/search";
+
+$.ajax({
+	url: searchResQueryURL,
+	headers: { "user-key": APIKey },
+	method: "GET"
+}).then(function(response) {
+	console.log(response);
+});
+
+// individual restaurant results
+// need to figure out how to get the restaurant id from the responses of restaurant options and input that in the URL here
+
+// var restaurantID =
+
+var individualRestURL =
+	"https://developers.zomato.com/api/v2.1/restaurant?res_id=16774318";
+
+$.ajax({
+	url: individualRestURL,
 	headers: { "user-key": APIKey },
 	method: "GET"
 }).then(function(response) {
@@ -94,7 +94,7 @@ $.ajax({
 	var lon = response.location.longitude;
 	// var map;
 	function initMap() {
-		var myLatLng =  { lat: parseFloat(lat), lng: parseFloat(lon) };
+		var myLatLng = { lat: parseFloat(lat), lng: parseFloat(lon) };
 
 		var map = new google.maps.Map(document.getElementById("map"), {
 			center: myLatLng,
@@ -105,42 +105,76 @@ $.ajax({
 			position: myLatLng,
 			map: map,
 			animation: google.maps.Animation.DROP
-        });
-        // google.maps.Data.StyleOptions.fillOpacity = 1;
-    	}
+		});
+	}
 	initMap();
 
 	// restaurant contact info
-    console.log("restaurant name: " + response.name);
-    $("#rName").text(response.name);
-    console.log("restaurant cuisine: " + response.cuisines);
-    $("#rCuisine").text(response.cuisines);
-    console.log("address: " + response.location.address);
-    $("#address").text(response.location.address);
-    console.log("phone number: " + response.phone_numbers);
-    $("#phoneNum").text(response.phone_numbers);
+	console.log("restaurant name: " + response.name);
+	$("#rName").text(response.name);
+	console.log("restaurant cuisine: " + response.cuisines);
+	$("#rCuisine").text(response.cuisines);
+	console.log("address: " + response.location.address);
+	$("#address").text(response.location.address);
+	console.log("phone number: " + response.phone_numbers);
+	$("#phoneNum").text(response.phone_numbers);
 
 	// restaurant website links
-    console.log("website: " + response.url);
-    $("#rWebsite").attr("href", response.url);
+	console.log("website: " + response.url);
+	$("#rWebsite").attr("href", response.url);
 	console.log("menu: " + response.menu_url);
 
 	// restaurant rating and price info
 	console.log("rating text: " + response.user_rating.rating_text);
-    console.log("aggregate rating: " + response.user_rating.aggregate_rating);
-    $("#rating").text("Rating: " + response.user_rating.aggregate_rating);
-    console.log("price range: " + response.price_range);
-    $("#price").text("Price Range: " + response.price_range);
+	console.log("aggregate rating: " + response.user_rating.aggregate_rating);
+
+	console.log("cost for two: " + response.average_cost_for_two);
+	$("#cost").text("Average Cost for Two: $" + response.average_cost_for_two);
+
+	$("#rating").text("Rating: " + response.user_rating.aggregate_rating);
+	
+	var rating = response.user_rating.aggregate_rating;
+
+	if (rating === 5) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_5.png")
+	}
+	else if (rating < 5 && rating >= 4.5) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_4_half.png")
+	}
+	else if (rating >= 4 && rating <= 4.5) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_4@2x.png")
+	}
+	else if (rating >= 3.5 && rating <= 4) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_3_half.png")
+	}
+	else if (rating >= 3 && rating <= 3.5) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_3.png")
+	}
+	else if (rating >= 2.5 && rating <= 3) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_2_half.png")
+	}
+	else if (rating >= 2 && rating <= 2.5) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_2.png")
+	}
+	else if (rating >= 1.5 && rating <= 2) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_1_half.png")
+	}
+	else if (rating >= 1 && rating <= 1.5) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_1.png")
+	}
+	else if (rating < 1) {
+		$("#rating").attr("src", "assets/yelp_stars/web_and_ios/small/small_0.png")
+	}
+
 
 	// restaurant photos
-    console.log("feautured photo url: " + response.thumb);
-    $("#featuredImg").attr("src", response.thumb);
-    console.log("photo gallery urls: ", response.photos);
-    $("#photo1").attr("src", response.photos[0].photo.thumb_url)
-    $("#photo2").attr("src", response.photos[1].photo.thumb_url)
-    $("#photo3").attr("src", response.photos[2].photo.thumb_url)
-    $("#photo4").attr("src", response.photos[3].photo.thumb_url)
-    $("#photo5").attr("src", response.photos[4].photo.thumb_url)
-    $("#photo6").attr("src", response.photos[5].photo.thumb_url)
+	console.log("feautured photo url: " + response.thumb);
+	$("#featuredImg").attr("src", response.thumb);
+	console.log("photo gallery urls: ", response.photos);
+	$("#photo1").attr("src", response.photos[0].photo.thumb_url);
+	$("#photo2").attr("src", response.photos[1].photo.thumb_url);
+	$("#photo3").attr("src", response.photos[2].photo.thumb_url);
+	$("#photo4").attr("src", response.photos[3].photo.thumb_url);
+	$("#photo5").attr("src", response.photos[4].photo.thumb_url);
+	$("#photo6").attr("src", response.photos[5].photo.thumb_url);
 });
-
